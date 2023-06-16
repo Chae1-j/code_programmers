03745ace8d65f276726349f473c47cd3a5aa846b
Chaewon Jeong <jcw3241@gmail.com>
2023-06-16 23:26:05 +0900

    0616 코딩테스트2문제
    가까운수 다시보기

--- src/codingTest_Intro/가까운수.java
+++ src/codingTest_Intro/가까운수.java
@@ -12,20 +12,35 @@ public class 가까운수 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] array = {10,11,12,5,26,7,33};
-		int answer = 0;
-		int close = array[0]-n; 
-		if(close < 0) {
-			close = -close; // 원소와 n의 차이가 음수 일 경우 양수로 변경.
-		}
 		
-		for(int i = 0; i < array.length; i++) {
-			if(array[i]-n < close) {
-				close = array[i] - n;
-			}
-			answer = array[i];
-			
-		}
-		System.out.println(answer);
+		int answer = array[0];  // 가장 가까운 수 초기값을 첫 번째 요소로 설정
+        int minDiff = Math.abs(array[0] - n);  // 가장 가까운 수와 n의 차이 초기값 설정
+
+        for (int num : array) {
+            int diff = Math.abs(num - n);  // 현재 숫자와 n의 차이 계산
+            if (diff < minDiff) {
+                answer = num;  // 더 가까운 수로 업데이트
+                minDiff = diff;  // 최소 차이 업데이트
+            } else if (diff == minDiff && num < answer) {
+                // 차이가 동일한 경우 더 작은 수를 선택
+                answer = num;
+            }
+        }
+        
+//		int answer = 0;
+//		int close = array[0]-n; 
+//		if(close < 0) {
+//			close = -close; // 원소와 n의 차이가 음수 일 경우 양수로 변경.
+//		}
+//		
+//		for(int i = 0; i < array.length; i++) {
+//			if(array[i]-n < close) {
+//				close = array[i] - n;
+//			}
+//			answer = array[i];
+//			
+//		}
+//		System.out.println(answer);
 	}
 
 }
