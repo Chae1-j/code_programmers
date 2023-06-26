f0185c365ab2b368905855df214ecadd6b12ae54
Chaewon Jeong <jcw3241@gmail.com>
2023-06-26 23:55:05 +0900

    Update 삼각형의완성조건_2.java
    삼각형의 완성조건 풀이 완료

--- src/codingTest_Intro/삼각형의완성조건_2.java
+++ src/codingTest_Intro/삼각형의완성조건_2.java
@@ -9,16 +9,19 @@ public class 삼각형의완성조건_2 {
 		 * - 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 
 		 * 	 정수의 개수를 return하도록 solution 함수를 완성해주세요.
 		 */
-		int[] side = {};
+		int[] side = {11,7};
 		int answer = 0;
-		int len = 0;
-		if(side[0] > side[1]) {
-			len = side[1]*side[1];
-		} else if(side[0] < side[1]) {
-			
-		} else { // 이등변삼각형의 경
-			
+		int cnt = 0;
+		int max = side[0];
+		if(side[1] > max) max = side[1];
+		int min = Math.abs(side[0]-side[1]);
+		for(int i = min; i < max; i++) {
+			cnt++;
 		}
+		for(int i = max+1; i < side[0]+side[1]; i++) {
+			answer++;
+		}
+		answer = answer+cnt;
 		System.out.println(answer);
 	}
 
