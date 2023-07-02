ca21a49d0919648a40baaa6c2244999e00f5bd76
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-02 22:49:57 +0900

    0702
    문제 추가 & 풀이중

---
+++ src/codingTest_Intro/등수매기기.java
@@ -0,0 +1,23 @@
+package codingTest_Intro;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 등수매기기 {
+
+	public static void main(String[] args) {
+		/*
+		 * 영어 점수와 수학 점수의 평균 점수를 기준으로 학생들의 등수를 매기려고 합니다. 영어 점수와 수학 점수를 담은 2차원 
+		 * 정수 배열 score가 주어질 때, 영어 점수와 수학 점수의 평균을 기준으로 매긴 등수를 담은 배열을 return하도록 
+		 * solution 함수를 완성해주세요.
+		 */
+		int[][] score = {{},
+						 {},
+						 {},
+						 {}};
+		int[] answer = new int[score.length];
+		ArrayList<Integer> list = new ArrayList();
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
