0a75c448d07bf7fd0ad8ca3d285be12a87bd6adb
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-04 23:41:05 +0900

    0704
    문제 추가. 풀이중

--- src/codingTest_Intro/평행.java
+++ src/codingTest_Intro/평행.java
@@ -3,19 +3,19 @@ package codingTest_Intro;
 public class 평행 {
 
 	public static void main(String[] args) {
-		int x1 = dots[0][0];
-        int y1 = dots[0][1];
-        int x2 = dots[1][0];
-        int y2 = dots[1][1];
-        int x3 = dots[2][0];
-        int y3 = dots[2][1];
-        int x4 = dots[3][0];
-        int y4 = dots[3][1];
-        
-        double slope1 = (double) (y2 - y1) / (x2 - x1);
-        double slope2 = (double) (y4 - y3) / (x4 - x3);
-        
-        return slope1 == slope2 ? 0 : 1;
+//		int x1 = dots[0][0];
+//        int y1 = dots[0][1];
+//        int x2 = dots[1][0];
+//        int y2 = dots[1][1];
+//        int x3 = dots[2][0];
+//        int y3 = dots[2][1];
+//        int x4 = dots[3][0];
+//        int y4 = dots[3][1];
+//        
+//        double slope1 = (double) (y2 - y1) / (x2 - x1);
+//        double slope2 = (double) (y4 - y3) / (x4 - x3);
+//        
+//        return slope1 == slope2 ? 0 : 1;
 	}
 
 }
