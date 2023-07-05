dd3518c743eb5f4b172efbc30b815a412d00efd5
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-05 13:37:14 +0900

    0705 저주의 숫자3 완료
    런타임에러 해결. 리스트 사이즈 보다 값이 더 클 경우가 발생

--- src/codingTest_Intro/저주의숫자3.java
+++ src/codingTest_Intro/저주의숫자3.java
@@ -23,14 +23,15 @@ public class 저주의숫자3 {
 		int n = sc.nextInt();
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		int answer = 0; 
-		for(int i = 0; i < 150; i++) {
+		for(int i = 0; i < 200; i++) {
 			String s = String.valueOf(i);
 			if(i%3!=0 && !s.contains("3")) {
 				list.add(i);
 			}
 //			answer = list.indexOf(n); indexOf("값")이므로 해당 값의 인덱스를 반환하는 것임.
 		}
-		System.out.println(list);
+		
+		System.out.println(list.size());
 		answer = list.get(n-1); // index는 0부터 시작함을 유
 		System.out.println("answer = " + answer);
 	}
