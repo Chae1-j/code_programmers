d67923bc3ff09e4430dd4a1b5cfaa1fb23e855b5
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-10 21:25:03 +0900

    Update 평행.java
    평행풀이중ㅠㅠ

--- src/codingTest_Intro/평행.java
+++ src/codingTest_Intro/평행.java
@@ -24,22 +24,64 @@ public class 평행 {
 //        double slope2 = (double) (y4 - y3) / (x4 - x3);
 //        
 //        return slope1 == slope2 ? 0 : 1;
-		int[][] dots = {{1,4},
-						{9,2},
-						{3,8},
-						{11,6}};
+		int[][] dots = {{3,5},
+						{4,1},
+						{2,4},
+						{5,10}};
 		int answer = 0;
-		int first = dots[0][0];
-		int second = dots[0][0];
-		int third = dots[0][0];
-		int fourth = dots[0][0];
-		int x = 0;
-		int y = 0;
-		for(int i = 0; i < dots.length; i++) {
-			if(dots[i][0] > first) {
-				first = Math.max(dots[i][0],dots[i][0]);
-			}
-		}
+//		int first = dots[0][0];
+//		int second = dots[0][0];
+//		int third = dots[0][0];
+//		int fourth = dots[0][0];
+//		int x = 0;
+//		int y = 0;
+//		for(int i = 0; i < dots.length; i++) {
+//			if(dots[i][0] > first) {
+//				first = Math.max(dots[i][0],dots[i][0]);
+//			}
+//		}
+		
+//		int[] cnt = new int[6]; 
+//		
+//        int index = 0; 
+//
+//        for(int i = 0; i < 3; i++) {
+//            for(int j = i + 1; j < 4; j++) {
+//                int result = (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0]);
+//                cnt[index] = result;
+//                index++;
+//            }
+//        }
+//
+//        for(int i = 0; i < cnt.length-1 ; i++){
+//            for(int j = i+1; j < cnt.length; j++) {
+//                if (cnt[i] == cnt[j]) answer = 1;
+//            }
+//        }
+//	      int answer = 0;
+//	        float[] line = new float[6]; //기울기 세는 배열
+//
+//	        //index로 돌리면서 바로 채움 line에
+//	        int index = 0; 
+//
+//	        for(int i = 0; i < 3; i++) {
+//	            for(int j = i + 1; j < 4; j++) {
+//	                float result 
+//	                    = (float)(dots[i][1] - dots[j][1]) 
+//	                    / (float)(dots[i][0] - dots[j][0]);
+//	                line[index] = result;
+//	                index++;
+//	            }
+//	        }
+//
+//	        //평행 체크
+//	        for(int k = 0; k < line.length-1 ; k++){
+//	            for(int m=k+1; m<line.length;m++) {
+//	                if (line[k] == line[m]) return 1;
+//	            }
+//	        }
+        
+        
 		System.out.println(answer);
 	}
 
