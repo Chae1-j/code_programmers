6f73cb61cbbd98be07a91fd2bb851932f8c57d56
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-15 10:33:12 +0900

    0715 lv1 풀이 추가
    정수 제곱근 판별 풀이 완료

--- src/codingTest_LV1/정수제곱근판별.java
+++ src/codingTest_LV1/정수제곱근판별.java
@@ -14,11 +14,22 @@ public class 정수제곱근판별 {
 		long answer = -1;
 		
 		// 1. for문반복
-		for(long i = 1; i < n; i++) {
-			if(n/i==i) {
+		for(long i = 1; i <= n; i++) {
+			if((int)Math.sqrt(n)==i) {
 				answer = (i+1)*(i+1);
+//				answer = (int)Math.sqrt(i+1);
+				break; 
 			}
-		} // 시간초과 오류 발생. 큰 숫자가 들어오면 반복문의 반복시간이 길어지므로 다른 방 필요
+		} // 시간초과 오류 발생. 큰 숫자가 들어오면 반복문의 반복시간이 길어지므로 다른 방법 필요 > break 구문으로 해결
+		/*
+		 *         
+		long answer = -1;
+        Double sqrt = Math.sqrt(n);
+      
+        if(sqrt == sqrt.intValue()){
+            answer =  (long)Math.pow(sqrt + 1, 2);
+        }
+		 */
 		System.out.println(answer);
 
 	}
