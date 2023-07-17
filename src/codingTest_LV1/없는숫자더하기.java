25305af88ae8c5a50ecd0d3a747475dc995d3700
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-07-18 00:07:11 +0900

    Update 없는숫자더하기.java
    풀이 완료

--- src/codingTest_LV1/없는숫자더하기.java
+++ src/codingTest_LV1/없는숫자더하기.java
@@ -9,7 +9,7 @@ public class 없는숫자더하기 {
 		 * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. numbers에서 찾을 수 없는 0부터 
 		 * 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
 		 */
-		int[] numbers = {};
+		int[] numbers = {5,8,4,0,6,7,9};
 		int answer = -1;
 //		Arrays.sort(numbers);
 //		for(int i = 0; i < numbers.length; i++) {
@@ -17,10 +17,16 @@ public class 없는숫자더하기 {
 //                answer += i;
 //            }
 //		}
-		for (int n : numbers) {
-            answer -= n;
-        }
-
+		int sum = 0; 
+		int total = 0;
+		for(int i = 0; i<10; i++) {
+			sum+=i;
+		}
+        
+        for(int i = 0; i<numbers.length; i++) {
+			total += numbers[i];
+		}
+            answer = sum - total;
 		System.out.println(answer);
 	}
 
