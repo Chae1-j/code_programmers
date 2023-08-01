d1fc2b092b92751086e9c32a1c93c7edfdb9ea0c
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-01 23:22:44 +0900

    0801 특이한 정렬 풀이 완료
    풀이 완료

--- src/codingTest_Intro/특이한정렬.java
+++ src/codingTest_Intro/특이한정렬.java
@@ -16,15 +16,34 @@ public class 특이한정렬 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] answer = new int[numlist.length];
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for(int i = 0; i < numlist.length; i++) {
-//			if(numlist[i]==n) list.add(numlist[i]);
-			list.add(Math.abs(n-numlist[i]));
-			for(int j = 0; j < list.size(); j++) {
-				if(list.get(i) <list.get(j)) answer[i]++;
-			}
-			System.out.println(list);
-		}
+//		ArrayList<Integer> list = new ArrayList<Integer>();
+//		for(int i = 0; i < numlist.length; i++) {
+////			if(numlist[i]==n) list.add(numlist[i]);
+//			list.add(Math.abs(n-numlist[i]));
+//			for(int j = 0; j < list.size(); j++) {
+//				int tmp = 0;
+//				if(list.get(i) <list.get(j)) {
+//					
+//				}
+//			}
+//			System.out.println(list);
+//		}
+        double[] order = new double[numlist.length];
+        
+        for(int i = 0; i < numlist.length; i++) {
+            if(numlist[i] - n < 0) 
+                // n을 을 때 음수 값인 경우, +0.5를 해서 같은 거리에 있는 양수 값보다 정렬을 했을 시 뒤로 밀리도록 함 
+                order[i] = ((int)Math.abs(numlist[i] - n)) + 0.5;
+            else 
+                order[i] = numlist[i] - n; 
+        }
+        
+        Arrays.sort(order); // 오름차순 정렬
+        
+        for(int i = 0; i < numlist.length; i++) {
+            if(order[i] % 1 != 0) answer[i] = n - (int)order[i];
+            else answer[i] = (int)order[i] + n;
+        }
 		System.out.println(Arrays.toString(answer));
 	}
 
