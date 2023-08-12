57e3649f50c7fdd6e8c3376fe02f1646e9bd736d
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-12 17:58:03 +0900

    0812 코딩테스트lv1 2문제
    - 행렬의 덧셈 풀이 완료
    - 최대공약수와 최소공배수 테스트케이스 통과 확인 > 제출 후 오류 발생. 확인예정

---
+++ src/codingTest_LV1/최대공약수와최소공배수.java
@@ -0,0 +1,41 @@
+package codingTest_LV1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 최대공약수와최소공배수 {
+
+	public static void main(String[] args) {
+		/*
+		 * 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요. 배열의 맨 앞에 최대공약수, 그다음 
+		 * 최소공배수를 넣어 반환하면 됩니다. 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 
+		 * 반환해야 합니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		ArrayList<Integer> list1 = new ArrayList<Integer>();		
+		ArrayList<Integer> list2 = new ArrayList<Integer>();
+		
+		int[] answer = new int[2];
+		for(int i = 1; i <= n; i++) {
+			if(n%i==0) list1.add(i);
+		}
+		for(int i = 1; i <= m; i++) {
+			if(m%i==0) list2.add(i);
+		}
+		for(int i = 0; i < list1.size(); i++) {
+			for(int j = 0; j < list2.size();j++) {
+				if(list1.get(i)==list2.get(j)) answer[0] = list1.get(i); 
+			}
+		}
+		if(m%n==0) {
+			answer[1] = m;
+		} else {
+			answer[1] = n*m;
+		}
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
