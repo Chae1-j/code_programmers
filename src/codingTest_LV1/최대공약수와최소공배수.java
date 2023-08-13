3d8abb591d9fbbfc08816ff1add38a74f9dfb8bd
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-13 21:44:08 +0900

    0813 lv1. 최대공약수와 최소공배수
     - 유클리드 호제법

--- src/codingTest_LV1/최대공약수와최소공배수.java
+++ src/codingTest_LV1/최대공약수와최소공배수.java
@@ -15,27 +15,36 @@ public class 최대공약수와최소공배수 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		ArrayList<Integer> list1 = new ArrayList<Integer>();		
-		ArrayList<Integer> list2 = new ArrayList<Integer>();
-		
-		int[] answer = new int[2];
-		for(int i = 1; i <= n; i++) {
-			if(n%i==0) list1.add(i);
-		}
-		for(int i = 1; i <= m; i++) {
-			if(m%i==0) list2.add(i);
-		}
-		for(int i = 0; i < list1.size(); i++) {
-			for(int j = 0; j < list2.size();j++) {
-				if(list1.get(i)==list2.get(j)) answer[0] = list1.get(i); 
-			}
-		}
-		if(m%n==0) {
-			answer[1] = m;
-		} else {
-			answer[1] = n*m;
-		}
-		System.out.println(Arrays.toString(answer));
+//		ArrayList<Integer> list1 = new ArrayList<Integer>();		
+//		ArrayList<Integer> list2 = new ArrayList<Integer>();
+//		
+//		int[] answer = new int[2];
+//		for(int i = 1; i <= n; i++) {
+//			if(n%i==0) list1.add(i);
+//		}
+//		for(int i = 1; i <= m; i++) {
+//			if(m%i==0) list2.add(i);
+//		}
+//		for(int i = 0; i < list1.size(); i++) {
+//			for(int j = 0; j < list2.size();j++) {
+//				if(list1.get(i)==list2.get(j)) answer[0] = list1.get(i); 
+//			}
+//		}
+//		if(m%n==0) {
+//			answer[1] = m;
+//		} else {
+//			answer[1] = n*m;
+//		}
+//		System.out.println(Arrays.toString(answer));
+//	}
+		int[] answer = {0,0};
+        answer[0]=gcd(Math.max(n,m), Math.min(n,m)); // 최대공약수
+        answer[1]=(n*m)/answer[0]; // 최소공배수
+        System.out.println(answer);
 	}
-
+	public static int gcd(int a, int b){
+		if(b==0) return a;
+		return gcd(b,a%b);
+	} //유클리드 호제법  
 }
+
