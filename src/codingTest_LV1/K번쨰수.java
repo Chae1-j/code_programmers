5efe2425bffbad6b2e2afdb30cc0f5ed021b29c1
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-18 15:07:33 +0900

    0818 k번째 수 완료
    -풀이완료

--- src/codingTest_LV1/K번쨰수.java
+++ src/codingTest_LV1/K번쨰수.java
@@ -17,16 +17,15 @@ public class K번쨰수 {
 		int[] array = {1, 5, 2, 6, 3, 7, 4};
 		int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 		int[] answer = new int[commands.length];
-		for(int i = 0; i < commands.length; i++) {
-			if(commands[i][1]-commands[i][0] != 0) {
-				int[] arr = Arrays.copyOfRange(array, commands[i][0]-1,commands[i][1]);
-				System.out.println(Arrays.toString(arr));
-				Arrays.sort(arr);
-				answer[i] = arr[commands[i][2]-1];
-				
-			} else {
-				
-			}
+		for (int i = 0; i < commands.length; i++) {
+	        int[] tmp = new int[commands[i][1] - commands[i][0] + 1];
+
+	        for (int j = 0; j <= commands[i][1] - commands[i][0]; j++) {
+	            tmp[j] = array[j + commands[i][0] - 1];
+	        }
+
+	        Arrays.sort(tmp);
+	        answer[i] = tmp[commands[i][2]-1];
 		}
 		System.out.println(Arrays.toString(answer));
 		
