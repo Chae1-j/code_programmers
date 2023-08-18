a5ac0771edcc8a704396805bc87d5cf13a5e98f9
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-19 08:21:58 +0900

    0819 프로그래머스 lv0. 특이한 정렬 완료.
    - 절대값이용

--- src/codingTest_Intro/특이한정렬.java
+++ src/codingTest_Intro/특이한정렬.java
@@ -13,62 +13,25 @@ public class 특이한정렬 {
 		 * 오도록 배치합니다. 정수가 담긴 배열 numlist와 정수 n이 주어질 때 numlist의 원소를 n으로부터 가까운 
 		 * 순서대로 정렬한 배열을 return하도록 solution 함수를 완성해주세요.
 		 */
-		int[] numlist = {1,2,3,4,5};
+		int[] numlist = {1,2,3,4,5,6};
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] answer = new int[numlist.length];
-//		ArrayList<Integer> list = new ArrayList<Integer>();
-//		for(int i = 0; i < numlist.length; i++) {
-////			if(numlist[i]==n) list.add(numlist[i]);
-//			list.add(Math.abs(n-numlist[i]));
-//			for(int j = 0; j < list.size(); j++) {
-//				int tmp = 0;
-//				if(list.get(i) <list.get(j)) {
-//					
-//				}
-//			}
-//			System.out.println(list);
-//		}
-//        double[] order = new double[numlist.length];
-//        
-//        for(int i = 0; i < numlist.length; i++) {
-//            if(numlist[i] - n < 0) 
-//                // n을 을 때 음수 값인 경우, +0.5를 해서 같은 거리에 있는 양수 값보다 정렬을 했을 시 뒤로 밀리도록 함 
-//                order[i] = ((int)Math.abs(numlist[i] - n)) + 0.5;
-//            else 
-//                order[i] = numlist[i] - n; 
-//        }
-//        
-//        Arrays.sort(order); // 오름차순 정렬
-//        
-//        for(int i = 0; i < numlist.length; i++) {
-//            if(order[i] % 1 != 0) answer[i] = n - (int)order[i];
-//            else answer[i] = (int)order[i] + n;
-//        }
-		
-		List<Integer> list = new ArrayList<>();
-        List<Integer> answer1 = new ArrayList<>();
-        for (Integer num : numlist) list.add(num);
-        
-        int len = list.size();
-        for(int j = 0 ; j <  len; j++){
-            int minDif = 10000;
-            int minNum = 0;
-            
-            for(int i = 0 ; i < list.size() ; i++){
-                if(Math.abs(minDif) > Math.abs(list.get(i) - n)){
-                    minNum = list.get(i);
-                    minDif = Math.abs(list.get(i) - n);
-                }
-                else if(Math.abs(minDif) == Math.abs(list.get(i) - n)){
-                    System.out.println("list.get(i)");
-                    if(minNum < list.get(i)) minNum = list.get(i);
-                }
-            }
-            answer1.add(minNum);
-            list.remove(Integer.valueOf(minNum));
+		double[] abs = new double[numlist.length];
+        for (int i=0; i<numlist.length; i++) {
+            if (numlist[i] - n < 0) 
+                abs[i] = Math.abs(numlist[i]-n) + 0.5;
+            else
+                abs[i] = numlist[i] - n;
         }
-		
+        Arrays.sort(abs);
+        for (int i=0; i<numlist.length; i++) {
+            if (abs[i]%1!=0)
+                answer[i] = n - (int)abs[i];
+            else
+                answer[i] = n + (int)abs[i];
+        }
+	
 		System.out.println(Arrays.toString(answer));
 	}
 
