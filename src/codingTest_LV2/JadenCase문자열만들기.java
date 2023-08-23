04cce24c8ea06eb8962426c60395a5a252998df9
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-23 22:43:11 +0900

    0823 JadenCase 문자열 풀이중
    - 런타임오류 해결완료 하였으나 실패. 원인파악 중

--- src/codingTest_LV2/JadenCase문자열만들기.java
+++ src/codingTest_LV2/JadenCase문자열만들기.java
@@ -10,16 +10,19 @@ public class JadenCase문자열만들기 {
 		 * 알파벳은 소문자로 쓰면 됩니다. (첫 번째 입출력 예 참고)
 		 * 문자열 s가 주어졌을 때, s를 JadenCase로 바꾼 문자열을 리턴하는 함수, solution을 완성해주세요.
 		 */
-		String s = "3people unFollowed me";
+		String s = "3people   unFollowed me";
 		String answer = "";
+		StringBuffer sb = new StringBuffer();
 		String[] sArr = s.split(" ");
 		for(String str : sArr) {
-			str = str.substring(0,1).toUpperCase() + str.substring(1,str.length()).toLowerCase();
-			answer = answer + str + " ";
-			answer.trim();
+			if(!str.isEmpty()) {
+				str = str.substring(0,1).toUpperCase() + str.substring(1,str.length()).toLowerCase();
+				System.out.println("반복문 내의 str : " + str);
+				sb.append(str);
+				sb.append(" ");
+			} // 공백이 여러개일 경우 empty 판단
 		}
-		
-		System.out.println(Arrays.toString(sArr));
+		answer = sb.toString().trim();
 		System.out.println(answer);
 	}
 
