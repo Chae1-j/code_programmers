a6c285532f43716097e27408e946e82faf437743
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-25 15:39:10 +0900

    0825 lv0.옹알이(1) & lv1. 같은숫자는 싫어 풀이
    - lv0(입문테스트) 문제 전체 풀이 완료
    - lv1. 다른 풀이 방법 찾아보기

--- src/codingTest_Intro/옹알이_1.java
+++ src/codingTest_Intro/옹알이_1.java
@@ -1,5 +1,7 @@
 package codingTest_Intro;
 
+import java.util.Arrays;
+
 public class 옹알이_1 {
 
 	public static void main(String[] args) {
@@ -14,32 +16,48 @@ public class 옹알이_1 {
 		 * 	 즉, 각 문자열의 가능한 모든 부분 문자열 중에서 "aya", "ye", "woo", "ma"가 한 번씩만 등장합니다.
 		 * - 문자열은 알파벳 소문자로만 이루어져 있습니다.
 		 */
-		String[] babbling = {"ayaye", "uuuma", "ye", "yemawoo", "ayaa"};
+		String[] babbling = {"aya", "yee", "u", "maa", "wyeoo"};
 		int answer = 0;
 		String[] words = {"aya","ye","woo","ma"};
 		for(int i = 0; i < babbling.length; i++) {
 			for(int j = 0; j < words.length; j++) {
-				if(babbling[i].equals(words[j])) {
-					answer++;
+				if(babbling[i].contains(words[j])) {
+					babbling[i]=babbling[i].replace(words[j], "@");
+					// words 의 발음들을 @(임의의 문자)로 치환
 				} 
 			}
+			System.out.println(Arrays.toString(babbling));
+			babbling[i] = babbling[i].replace("@", "");
+			// 치환한 문자를 공백으로 다시 치환 > 발음가능한 문자들만 모인 단어들인지 판단하기 위한 처리작업
+			if(babbling[i].equals("")) answer++; // 공백만 있는 경우에 단어의 갯수 증가처리
 		}
 		
+		System.out.println(Arrays.toString(babbling));
 		System.out.println(answer);
-//		String[] banList = {"ayaaya", "yeye", "woowoo", "mama"};
-//	    String[] speakingList = {"aya", "ye", "woo", "ma"}; // 전역변수
-//        for (String str : babbling) {
-//            String tempStr = str;
-//            for (String ban : banList) {
-//                tempStr = tempStr.replace(ban, "z");
-//            }
-//            for (String speak : speakingList) {
-//                tempStr = tempStr.replace(speak, "");
-//            }
-//            if (tempStr.length() == 0) {
-//                answer++;
-//            }
-//        }
+
   	}
 
 }
+
+/*
+ * 
+ *		리스트와 배열을 이용한 방법(다른사람 풀이참조)
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int solution(String[] babbling) {
+        int count = 0;
+        // 문자열 배열을 담을 리스트 생성
+        List<String[]> list = new ArrayList<>();
+        for (int i = 0; i < babbling.length; i++) {
+            // 문자열 split으로 자른다. 만약 이 안에 있는 원소로만 잘린다면 빈칸이 됨
+            String[] temp = babbling[i].split("aya|ye|woo|ma");
+            // 문자열 배열을 리스트에 넣는다.
+            list.add(temp);
+        }
+        // 리스트 안의 문자열 배열의 빈칸의 갯수를 셈 (빈칸 == 모든 문자가 다 사라짐을 의미)
+        for (int i = 0; i < list.size(); i++) if (list.get(i).length == 0) count++;
+        return count;
+    }
+}
+ */
