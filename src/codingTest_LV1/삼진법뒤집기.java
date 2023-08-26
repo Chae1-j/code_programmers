2830698e41016d01693881976fc9e3814ef48915
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-08-26 23:30:14 +0900

    0826 lv1. 3진법 뒤집기 문제 추가
    - 풀이 진행 중

---
+++ src/codingTest_LV1/삼진법뒤집기.java
@@ -0,0 +1,35 @@
+package codingTest_LV1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class 삼진법뒤집기 {
+
+	public static void main(String[] args) {
+		/*
+		 * 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 
+		 * 수를 return 하도록 solution 함수를 완성해주세요.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int answer = 1;
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		while(n>0) {
+			list.add(n%3);
+			n /= 3;
+		}
+		Collections.reverse(list);
+		for(int i = 0; i < list.size();i++) {
+			if(list.get(i)!= 0) {
+				for(int j = 1; j <= i; j++) {
+					answer *= 3;
+				}
+				answer *= list.get(i);
+			}
+		}
+		if(list.get(0)!= 0) answer += 1;
+		System.out.println("answer : " + answer);
+	}
+
+}
