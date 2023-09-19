86c7956a0b44875117adfbe1fe93efe28826d6ec
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-09-19 20:23:42 +0900

    0919 lv1. 모의고사 풀이 완료
    - 완전탐색 문제
    - 반복문, arraylist이용

--- src/codingTest_LV1/모의고사.java
+++ src/codingTest_LV1/모의고사.java
@@ -1,6 +1,8 @@
 package codingTest_LV1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 모의고사 {
 
@@ -14,8 +16,28 @@ public class 모의고사 {
 		 * 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 
 		 * 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 		 */
-		int[] answers = {};
+		int[] answers = {1,3,2,4,2};
 		int[] answer = {};
+        int[] first = {1,2,3,4,5}; 
+        int[] second = {2,1,2,3,2,4,2,5}; 
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        int[] score = {0,0,0}; // 각 수포자들의 점수
+        
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i] == first[i%5]) score[0]++;
+            if(answers[i] == second[i%8]) score[1]++;
+            if(answers[i] == third[i%10]) score[2]++;
+        }
+        
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i=0; i<score.length; i++) if(max == score[i]) list.add(i+1);
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            answer[i] = list.get(i);
+        }
 		
 		System.out.println(Arrays.toString(answer));
 	}
