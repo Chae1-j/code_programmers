725a3ec2ee8b8c6b4e76357eb9424ad099527537
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-10-28 22:57:00 +0900

    1028 프로그래머스 lv1. 로또의 최고순위와 최저순위
    - for문
    - if문 & switch-case문 이용
     > 판별방법 생각해보기

---
+++ src/codingTest_lv1/로또의최고순위와최저순위.java
@@ -0,0 +1,68 @@
+package codingTest_lv1;
+
+import java.util.Arrays;
+
+public class 로또의최고순위와최저순위 {
+
+	private static final int case1 = 0;
+
+	public static void main(String[] args) {
+		/*
+		 * 로또를 구매한 민우는 당첨 번호 발표일을 학수고대하고 있었습니다. 하지만, 민우의 동생이 로또에 낙서를 하여, 일부 번호를 
+		 * 알아볼 수 없게 되었습니다. 당첨 번호 발표 후, 민우는 자신이 구매했던 로또로 당첨이 가능했던 최고 순위와 최저 순위를 
+		 * 알아보고 싶어 졌습니다. 알아볼 수 없는 번호를 0으로 표기하기로 하고, 민우가 구매한 로또 번호 6개가 44, 1, 0, 0, 
+		 * 31 25라고 가정해보겠습니다. 당첨 번호 6개가 31, 10, 45, 1, 6, 19라면, 당첨 가능한 최고 순위와 최저 순위의 한 
+		 * 예는 아래와 같습니다.
+		 * 	- 순서와 상관없이, 구매한 로또에 당첨 번호와 일치하는 번호가 있으면 맞힌 걸로 인정됩니다.
+		 * 	- 알아볼 수 없는 두 개의 번호를 각각 10, 6이라고 가정하면 3등에 당첨될 수 있습니다. > 3등을 만드는 다른 방법들도 
+		 * 	  존재합니다. 하지만, 2등 이상으로 만드는 것은 불가능합니다.
+		 * 	- 알아볼 수 없는 두 개의 번호를 각각 11, 7이라고 가정하면 5등에 당첨될 수 있습니다. > 5등을 만드는 다른 방법들도 
+		 * 	  존재합니다. 하지만, 6등(낙첨)으로 만드는 것은 불가능합니다.
+		 * 민우가 구매한 로또 번호를 담은 배열 lottos, 당첨 번호를 담은 배열 win_nums가 매개변수로 주어집니다. 이때, 당첨 
+		 * 가능한 최고 순위와 최저 순위를 차례대로 배열에 담아서 return 하도록 solution 함수를 완성해주세요.
+		 */
+		int[] lottos = {44, 1, 0, 0, 31, 25};
+		int[] win_nums = {31, 10, 45, 1, 6, 19};
+		int[] answer = new int[2];
+		Arrays.sort(lottos);
+		Arrays.sort(win_nums);
+		int cnt = 0;
+		int cnt2 = 0;
+		for(int i = 0; i < 6; i++) {
+			if(lottos[i] == 0) cnt2++;
+			for(int j = 0; j < 6; j++) {
+				if(lottos[i] == win_nums[j]) cnt++; 
+			}
+		}
+		if(cnt == 0) {
+			answer[0] = 1;
+			answer[1] = 6;
+		} else if(cnt > 0 && cnt < 6) {
+			answer[0] = 1;
+			switch(cnt) {
+				case 1 : 
+					answer[1] = 6;
+					break;
+				case 2 :
+					answer[1] = 5;
+					break;
+				case 3 : 
+					answer[1] = 4;
+					break;
+				case 4 : 
+					answer[1] = 3;
+					break;
+				case 5 : 
+					answer[1] = 2;
+					break;
+			}
+		} else {
+			answer[0] = 1;
+			answer[1] = 1;
+		}
+		
+		System.out.println(cnt);
+		System.out.println(Arrays.toString(answer));
+	}
+
+}
