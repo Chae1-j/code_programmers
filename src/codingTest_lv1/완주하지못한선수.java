3de9008aa26131222e453e22e3e42835eedb1dca
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-11-14 23:44:01 +0900

    1114 코딩테스트lv1. 완주하지 못한 선수
    ** 이클립스내에서는 오류 없으나 프로그래머스 문제 내에서 미통과 > 원인 찾아보기 **
    - 정렬:sort 이용
    - for문: break활용

---
+++ src/codingTest_lv1/완주하지못한선수.java
@@ -0,0 +1,33 @@
+package codingTest_lv1;
+
+import java.util.Arrays;
+
+public class 완주하지못한선수 {
+
+	public static void main(String[] args) {
+		/*
+		 * 수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다. 
+		 * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
+		 * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
+		 */
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
+		String answer = "";
+		int cnt = 0;
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+//		System.out.println(Arrays.toString(participant));
+//		System.out.println(Arrays.toString(completion));
+		for(int i = 0; i < completion.length; i++) {
+			if((participant[i]!=(completion[i]))) {
+				answer = participant[i];
+				break;
+			} else {
+				cnt++;
+			}
+			if(cnt == completion.length) answer = participant[i+1];
+		}
+		System.out.println(answer);
+	}
+
+}
