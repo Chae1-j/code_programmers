726f5a4383a939e6432cb8df3d8f0ef94f24f1e3
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-11-15 23:03:10 +0900

    1115 lv1. 완주하지 못한 선수 문제 풀이 해결
    - equals 이용. (중간에 왜 변경했는지 의문?)
    - 이후 정확성 6번 미통과. 반례 : 단, 1명만 참여하고 그 1명이 통과하지 못한경우 > 배열길이로 배열의 비어있음 여부 판별 후 통과
    - 시간초과 > if, else의 조건식의 순서변경후 해결

--- src/codingTest_lv1/완주하지못한선수.java
+++ src/codingTest_lv1/완주하지못한선수.java
@@ -10,23 +10,29 @@ public class 완주하지못한선수 {
 		 * 마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 
 		 * 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 		 */
-		String[] participant = {"leo", "kiki", "eden"};
-		String[] completion = {"eden", "kiki"};
+		String[] participant = {"kiki", "kiki", "kiki",};
+		String[] completion = {"kiki", "kiki"};
 		String answer = "";
 		int cnt = 0;
 		Arrays.sort(participant);
 		Arrays.sort(completion);
 //		System.out.println(Arrays.toString(participant));
 //		System.out.println(Arrays.toString(completion));
-		for(int i = 0; i < completion.length; i++) {
-			if((participant[i]!=(completion[i]))) {
-				answer = participant[i];
-				break;
-			} else {
-				cnt++;
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		if(completion.length == 0) {
+            answer = participant[0];
+		} else {
+            for(int i = 0; i < completion.length; i++) {
+				if(!participant[i].equals(completion[i])) {
+					answer = participant[i];
+					break;
+				} else {
+					cnt++;
+				}
+				if(cnt == completion.length) answer = participant[i+1];
 			}
-			if(cnt == completion.length) answer = participant[i+1];
-		}
+        }
 		System.out.println(answer);
 	}
 
