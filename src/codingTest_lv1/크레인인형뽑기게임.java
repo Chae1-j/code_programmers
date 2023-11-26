df51bf971d96a824aae608608b2d3d19c21a8126
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-11-26 15:29:53 +0900

    1126 lv1.크레인 인형뽑기 풀이완료/ lv1. 신규아이디 추천 문제추가
    - stack 그대로 이용
     > add와 peek 추가 사용

--- src/codingTest_lv1/크레인인형뽑기게임.java
+++ src/codingTest_lv1/크레인인형뽑기게임.java
@@ -20,28 +20,32 @@ public class 크레인인형뽑기게임 {
 		 * 격자의 상태가 담긴 2차원 배열 board와 인형을 집기 위해 크레인을 작동시킨 위치가 담긴 배열 moves가 매개변수로 주어질 
 		 * 때, 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
 		 */
-		int[][] board = {{},{}};
-		int[] moves = {};
+		int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+		int[] moves = {1,5,3,5,1,2,1,4};
 		int answer = 0;
-        Stack<Integer> stack = new Stack<>();       
-        
-        for(int i =0;i<moves.length;i++){
-            for(int j=0;j<board.length;j++){
-                if(board[j][moves[i]-1] != 0 ) {                   
-                    if(!stack.empty() && stack.peek() == board[j][moves[i]-1]){
-                        answer++;
-                        stack.pop();
-                        board[j][moves[i]-1] = 0;
-                        break;
-                    }else{
-                        stack.push(board[j][moves[i]-1]);                      
-                        board[j][moves[i]-1] = 0;
-                        break;
-                    }
-                }                        
-            }
-        }
-		
+		Stack<Integer> stack = new Stack<Integer>();
+
+		for (int i = 0; i < moves.length; i++) {
+			int tmp = moves[i] - 1;
+
+			for (int j = 0; j < board[tmp].length; j++) {
+				if (board[j][tmp] != 0) {
+					int key = board[j][tmp];
+					board[j][tmp] = 0;
+					if (!stack.isEmpty()) {
+						if (key == stack.peek()) {
+							stack.pop();
+							answer+=2;
+						} else {
+							stack.add(key);
+						}
+					} else {
+						stack.add(key);
+					}
+					break;
+				}
+			}
+		}
 		System.out.println(answer);
 	}
 
