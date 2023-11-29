6dcce92121c73520074541324054bf3f9c51f1fa
Chaewon Jeong <114040566+Chae1-j@users.noreply.github.com>
2023-11-29 23:09:06 +0900

    1129 lv1. 성격유형검사하기 문제추가, 풀이 중

---
+++ src/codingTest_lv1/성격유형검사하기.java
@@ -0,0 +1,19 @@
+package codingTest_lv1;
+
+public class 성격유형검사하기 {
+
+	public static void main(String[] args) {
+		/*
+		 * 나만의 카카오 성격 유형 검사지를 만들려고 합니다.
+		 * 성격 유형 검사는 다음과 같은 4개 지표로 성격 유형을 구분합니다. 성격은 각 지표에서 두 유형 중 하나로 결정됩니다.
+		 * 질문마다 판단하는 지표를 담은 1차원 문자열 배열 survey와 검사자가 각 질문마다 선택한 선택지를 담은 1차원 정수 배열 
+		 * choices가 매개변수로 주어집니다. 이때, 검사자의 성격 유형 검사 결과를 지표 번호 순서대로 return 하도록 solution 
+		 * 함수를 완성해주세요.
+		 */
+		String[] survey = {};
+		int[] choices = {};
+		String answer = "";
+		System.out.println(answer);
+	}
+
+}
